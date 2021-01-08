b8fc3b52db9380e051d124d3b529350654dc19c1
sharadonly <ssharad@gmail.com>
2021-01-08 17:39:07 -0500

    p2
    p2

---
+++ java/Assignment2/TrafficLightComponent.java
@@ -0,0 +1,15 @@
+import javax.swing.JComponent;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+
+public class TrafficLightComponent extends JComponent
+{
+   public void paintComponent(Graphics g)
+   {
+      Graphics2D g2 = (Graphics2D) g;
+      // your work here
+      . . .
+   }
+}
